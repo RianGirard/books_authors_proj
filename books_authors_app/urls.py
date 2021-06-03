8dcc85df2e0b12d9fa37ae1a7831de7f8806799c
Rian Girard <riangirard@gmail.com>
2021-06-03 11:16:49 -0700

    project updates

--- books_authors_app/urls.py
+++ books_authors_app/urls.py
@@ -3,4 +3,11 @@ from . import views
 
 urlpatterns = [
     path('', views.index),
+    path('add_book', views.add_book),
+    path('books/<int:id>', views.book_detail),
+    path('assign_author', views.assign_author),
+    path('authors', views.authors),
+    path('add_author', views.add_author),
+    path('authors/<int:id>', views.author_detail),
+    path('assign_book', views.assign_book),
 ]
--- books_authors_app/views.py
+++ books_authors_app/views.py
@@ -1,8 +1,63 @@
-from django.shortcuts import render #, redirect
+from django.shortcuts import render, redirect
+from .models import Book, Author
 
 def index(request):
-    
-    return render(request, "index.html")
+    context = {
+        "all_books": Book.objects.all(),
+    }
+    return render(request, "index.html", context)
 
+def add_book(request):
+    if request.method == "POST":
+        Book.objects.create(title=request.POST['title'], desc=request.POST['desc'])
+
+    return redirect('/')
+
+def book_detail(request, id):
+    if request.method == "POST":
+        context = {
+            "book": Book.objects.get(id=request.POST['book_id']),
+            "authors": Author.objects.exclude(books=Book.objects.get(id=request.POST['book_id'])),
+        }
+
+    return render(request, 'books.html', context)
+
+def assign_author(request):
+    if request.method == "POST":
+        book = Book.objects.get(id=request.POST['book_id'])             # many-to-many requires instantiate both and then add
+        author = Author.objects.get(id=request.POST['new_author'])
+        book.authors.add(author)
+    
+    return redirect('/authors')     # a redirect requires the /slash! 
+
+def authors(request):
+    context = {
+        "all_authors": Author.objects.all(),
+    }
+    return render(request, "authors.html", context)
+
+
+def add_author(request): 
+    if request.method == "POST":
+        Author.objects.create(first_name=request.POST['f_name'], last_name=request.POST['l_name'], notes=request.POST['notes'])
+
+    return redirect('/')
+
+def author_detail(request, id):
+    if request.method == "POST":
+        context = {
+            "author": Author.objects.get(id=request.POST['author_id']),
+            "books": Book.objects.exclude(authors=Author.objects.get(id=request.POST['author_id'])),
+        }
+
+    return render(request, 'author_detail.html', context)
+
+def assign_book(request):
+    if request.method == "POST":
+        book = Book.objects.get(id=request.POST['new_book'])             # many-to-many requires instantiate both and then add
+        author = Author.objects.get(id=request.POST['author_id'])
+        book.authors.add(author)
+    
+    return redirect('/authors')     # a redirect requires the /slash! 
 
 # Create your views here.
